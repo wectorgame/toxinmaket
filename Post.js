2c4060be3ebcd42be91e9e906ce8d7efbaa6480f
wectorgame <wectorgame@gmail.com>
2020-09-26 19:16:20 +0300

    adding webpack
    +some js functions

---
+++ Post.js
@@ -0,0 +1,12 @@
+class Post{
+    constructor(title){
+        this.title = title
+        this.date = new Date()
+    }
+    toString(){
+        JSON.stringify({
+            title: this.title,
+            date: this.date.toJSON()
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+const post = new Post('Toxinmaket post title')
+console.log('Post to String:', post.toString())
