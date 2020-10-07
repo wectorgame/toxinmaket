50a3352dccaf5713d64f3225cc8ce14ccc5dfc03
wectorgame <wectorgame@gmail.com>
2020-10-07 12:49:44 +0300

    add login form

--- index.jsx
+++ index.jsx
@@ -17,217 +17,233 @@ console.log("Post to String:", post.toString());
 
 const App = () => (
   <>
-  <Cards/>
-  <Login/>
-  <div class="container">
-    <div class="navbar">
-      <img src="img/Logo.png" alt="" class="nav-img" />
+    <LogIn />
+    <Cards />
 
-      <ul class="menu-list">
-        <li class="active">
-          <a href="#"> О нас </a>
-        </li>
-        <li class="nav-services">
-          <a href="#" class="not-active">
-            {" "}
-            Услуги
-          </a>
-          <ul class="sub-menu-1">
-            <li>
-              <a href="#"> Жилье</a>
-            </li>
-            <li>
-              <a href="#"> Заказ</a>
-            </li>
-            <li>
-              <a href="#"> Бронь</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" class="not-active">
-            {" "}
-            Вакансии
-          </a>
-        </li>
-        <li>
-          <a href="#" class="not-active">
-            {" "}
-            Новости
-          </a>
-        </li>
-        <li>
-          <a href="#" class="not-active">
-            Соглашение
-          </a>
-        </li>
-      </ul>
-      <button class="btn btn1">войти</button>
-      <button class="btn btn2">зарегистрироваться</button>
-    </div>
+    <div class="container">
+      <div class="navbar">
+        <img src="img/Logo.png" alt="" class="nav-img" />
 
-    <div class="navbar">
-      <img src="img/Logo.png" alt="" class="nav-img" />
-      <ul class="menu-list">
-        <li class="active">
-          <a href="#"> О нас </a>
-        </li>
-        <li class="nav-services">
-          <a href="#" class="not-active">
-            {" "}
-            Услуги
-          </a>
-          <ul class="sub-menu-1">
-            <li>
-              <a href="#"> Жилье</a>
-            </li>
-            <li>
-              <a href="#"> Заказ</a>
-            </li>
-            <li>
-              <a href="#"> Бронь</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" class="not-active">
-            {" "}
-            Вакансии
-          </a>
-        </li>
-        <li>
-          <a href="#" class="not-active">
-            {" "}
-            Новости
-          </a>
-        </li>
-        <li>
-          <a href="#" class="not-active">
-            Соглашение
-          </a>
-        </li>
-      </ul>
-      <div class="office">
-        <a href="#" class="not-active">
-          Юлий Цезарь
-        </a>
+        <ul class="menu-list">
+          <li class="active">
+            <a href="#"> О нас </a>
+          </li>
+          <li class="nav-services">
+            <a href="#" class="not-active">
+              {" "}
+              Услуги
+            </a>
+            <ul class="sub-menu-1">
+              <li>
+                <a href="#"> Жилье</a>
+              </li>
+              <li>
+                <a href="#"> Заказ</a>
+              </li>
+              <li>
+                <a href="#"> Бронь</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" class="not-active">
+              {" "}
+              Вакансии
+            </a>
+          </li>
+          <li>
+            <a href="#" class="not-active">
+              {" "}
+              Новости
+            </a>
+          </li>
+          <li>
+            <a href="#" class="not-active">
+              Соглашение
+            </a>
+          </li>
+        </ul>
+        <button class="btn btn1">войти</button>
+        <button class="btn btn2">зарегистрироваться</button>
       </div>
-    </div>
 
-    <footer class="footer-widgets">
-      <div className="footer-groups">
-        <div class="footer-group1">
-          <img src="img/Logo.png" alt="" />
-          <p class="footer-descp">
-            Бронирование номеров в лучшем отеле 2019 года по версии ассоциации
-            «Отельные взгляды»
-          </p>
+      <div class="navbar">
+        <img src="img/Logo.png" alt="" class="nav-img" />
+        <ul class="menu-list">
+          <li class="active">
+            <a href="#"> О нас </a>
+          </li>
+          <li class="nav-services">
+            <a href="#" class="not-active">
+              {" "}
+              Услуги
+            </a>
+            <ul class="sub-menu-1">
+              <li>
+                <a href="#"> Жилье</a>
+              </li>
+              <li>
+                <a href="#"> Заказ</a>
+              </li>
+              <li>
+                <a href="#"> Бронь</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" class="not-active">
+              {" "}
+              Вакансии
+            </a>
+          </li>
+          <li>
+            <a href="#" class="not-active">
+              {" "}
+              Новости
+            </a>
+          </li>
+          <li>
+            <a href="#" class="not-active">
+              Соглашение
+            </a>
+          </li>
+        </ul>
+        <div class="office">
+          <a href="#" class="not-active">
+            Юлий Цезарь
+          </a>
         </div>
-        <ul>
-          <li class="footer-header">Навигация</li>
-          <li class="not-active">О нас</li>
-          <li class="not-active">Новости</li>
-          <li class="not-active">Служба поддержки</li>
-          <li class="not-active">Услуги</li>
-        </ul>
-        <ul>
-          <li class="footer-header">О нас</li>
-          <li class="not-active">О сервисе</li>
-          <li class="not-active">Наша команда</li>
-          <li class="not-active">Вакансии</li>
-          <li class="not-active">Инвесторы</li>
-        </ul>
-        <ul>
-          <li class="footer-header">Служба поддержки</li>
-          <li class="not-active">Соглашения</li>
-          <li class="not-active">Сообщества</li>
-          <li class="not-active">Связь с нами</li>
-        </ul>
-        <div class="footer-group2">
-          <p class="footer-header">подписка</p>
-          <p class="footer-descp">
-            Получайте специальные предложения и новости сервиса
-          </p>
-          <div className="inputWithIcon">
-            <input type="text" placeholder="Email" class="input-email" />
-            <i class="material-icons">arrow_forward</i>
+      </div>
+
+      <footer class="footer-widgets">
+        <div className="footer-groups">
+          <div class="footer-group1">
+            <img src="img/Logo.png" alt="" />
+            <p class="footer-descp">
+              Бронирование номеров в лучшем отеле 2019 года по версии ассоциации
+              «Отельные взгляды»
+            </p>
+          </div>
+          <ul>
+            <li class="footer-header">Навигация</li>
+            <li class="not-active">О нас</li>
+            <li class="not-active">Новости</li>
+            <li class="not-active">Служба поддержки</li>
+            <li class="not-active">Услуги</li>
+          </ul>
+          <ul>
+            <li class="footer-header">О нас</li>
+            <li class="not-active">О сервисе</li>
+            <li class="not-active">Наша команда</li>
+            <li class="not-active">Вакансии</li>
+            <li class="not-active">Инвесторы</li>
+          </ul>
+          <ul>
+            <li class="footer-header">Служба поддержки</li>
+            <li class="not-active">Соглашения</li>
+            <li class="not-active">Сообщества</li>
+            <li class="not-active">Связь с нами</li>
+          </ul>
+          <div class="footer-group2">
+            <p class="footer-header">подписка</p>
+            <p class="footer-descp">
+              Получайте специальные предложения и новости сервиса
+            </p>
+            <div className="inputWithIcon">
+              <input type="text" placeholder="Email" class="input-email" />
+              <i class="material-icons pink-arrow ">arrow_forward</i>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="copyright">
-        <div className="copyright-text">
-          Copyright © 2018 Toxin отель. Все права зачищены.
+        <div className="copyright">
+          <div className="copyright-text">
+            Copyright © 2018 Toxin отель. Все права зачищены.
+          </div>
+
+          <a href="http://twitter.com">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="http://facebook.com">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+          <a href="http://instagram.com">
+            <i class="fab fa-instagram"></i>
+          </a>
         </div>
+      </footer>
+      <div className="empty"></div>
 
-        <a href="http://twitter.com">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="http://facebook.com">
-          <i class="fab fa-facebook-square"></i>
-        </a>
-        <a href="http://instagram.com">
-          <i class="fab fa-instagram"></i>
-        </a>
+      <div className="footer-small">
+        <div class="footer-small-group">
+          <img src="img/Logo.png" alt="" />
+          <p class="footer-small-descp">
+            Copyright © 2018 Toxin отель. Все права зачищены.
+          </p>
+          <a href="http://twitter.com">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="http://facebook.com">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+          <a href="http://instagram.com">
+            <i class="fab fa-instagram"></i>
+          </a>
+        </div>
       </div>
-    </footer>
-    <div className="empty"></div>
-
-    <div className="footer-small">
-      <div class="footer-small-group">
-        <img src="img/Logo.png" alt="" />
-        <p class="footer-small-descp">
-          Copyright © 2018 Toxin отель. Все права зачищены.
-        </p>
-        <a href="http://twitter.com">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="http://facebook.com">
-          <i class="fab fa-facebook-square"></i>
-        </a>
-        <a href="http://instagram.com">
-          <i class="fab fa-instagram"></i>
-        </a>
-      </div>
+      <div className="empty"></div>
     </div>
-    <div className="empty"></div>
-  </div>
   </>
 );
 
 const Cards = () => (
-  <div class="container">
-    <div className="card">
-      <div className="card-img">
-      <div className="ellipse">
-        <input type="radio" name="hotel" className="dot"/>
-        <input type="radio" name="hotel" className="dot"/>
-        <input type="radio" name="hotel" className="dot"/>
-        <input type="radio" name="hotel" className="dot"/>
-       
-      </div></div>
-      <div className="card-info">
-        <div className="card-number">№840</div>
-        <div className="card-price">9 900₽ в сутки</div>
-        <div className="card-border">
-        <div className="card-stars">
-        <i class="material-icons">star</i>
-        <i class="material-icons">star</i>
-        <i class="material-icons">star</i>
-        <i class="material-icons">star</i>
-        <i class="material-icons">star_border</i>
+  <>
+    <div class="container">
+      <div className="card">
+        <div className="card-img">
+          <div className="ellipse">
+            <input type="radio" name="hotel" className="dot" />
+            <input type="radio" name="hotel" className="dot" />
+            <input type="radio" name="hotel" className="dot" />
+            <input type="radio" name="hotel" className="dot" />
+          </div>
         </div>
-        <div className="card-reviews">65 Отзывов</div>
+        <div className="card-info">
+          <div className="card-number">№840</div>
+          <div className="card-price">9 900₽ в сутки</div>
+          <div className="card-border">
+            <div className="card-stars">
+              <i class="material-icons">star</i>
+              <i class="material-icons">star</i>
+              <i class="material-icons">star</i>
+              <i class="material-icons">star</i>
+              <i class="material-icons">star_border</i>
+            </div>
+            <div className="card-reviews">65 Отзывов</div>
+          </div>
         </div>
       </div>
-     
+    </div>
+  </>
+);
+const LogIn = () => (
+  <div class="container">
+    <div className="login-form">
+      <h1 className="login-title">Войти</h1>
+      <input type="text" placeholder="Email" class="login-email" />
+      <input type="text" placeholder="Пароль" className="login-pw" />
+      <div className="login-btn-with-icon">
+        <button type="submit" className="login-btn">
+          Войти
+          <i class="material-icons login-arrow">arrow_forward</i>
+        </button>
+      </div>
+      <div className="login-create">
+        <p className="login-btn-text">Нет аккаунта на Toxin?</p>
+        <button type="submit" className="login-btn-create">
+          Создать
+        </button>
+      </div>
     </div>
   </div>
 );
-const Login = ()=>{
-  <div className="container">
-    <h1 className="title">Войти</h1>
-  </div>
-}
 render(<App />, document.getElementById("app"));
-
-
